c1700d9ebc2dabda9b135baf1c534ff042128c6f
ckib16 <ckib16@gmail.com>
2015-10-26 21:21:46 -0400

    lists of tasks lefto do

--- script.js
+++ script.js
@@ -5,53 +5,90 @@ $(document).ready(function () {
   // console.log($('h1'))
   // console.log(question.q1)
 
+  // Question bank
+  // - object/hash of question numbers with choices and an answer
+  // - loop through bank to cycle thru questions
+  // - pass question to the printQuestion function
+
   function printQuestion () {
-    var question = {
-      q1: 'This is the text for q1',
-      c1: 'choice 1',
-      c2: 'choice 2',
-      c3: 'choice 3',
-      a: '1'
+    var questionBank = {
+      q1: {
+        t: 'This is the question #1 text',
+        c1: 'choice 1',
+        c2: 'choice 2',
+        c3: 'choice 3',
+        a: '1'
+      },
+      q2: {
+        t: 'This is the question #2 text',
+        c1: 'choice 1',
+        c2: 'choice 2',
+        c3: 'choice 3',
+        a: '2'
+      },
+      q3: {
+        t: 'This is the question #3 text',
+        c1: 'choice 1',
+        c2: 'choice 2',
+        c3: 'choice 3',
+        a: '3'
+      }
     }
-    // console.log(question.q1)
-    $('.questionDiv > p').html(question.q1)
-    $('.choiceDiv p:eq(0)').html(question.c1)
-    $('.choiceDiv p:eq(1)').html(question.c2)
-    $('.choiceDiv p:eq(2)').html(question.c3)
+
+    // var question = {
+    //   t: 'This is the text for the question',
+    //   c1: 'choice 1',
+    //   c2: 'choice 2',
+    //   c3: 'choice 3',
+    //   a: '1'
+    // }
+
+    $('.questionDiv > p').html(questionBank.q1.t)
+    $('.choiceDiv p:eq(0)').html(questionBank.q1.c1)
+    $('.choiceDiv p:eq(1)').html(questionBank.q1.c2)
+    $('.choiceDiv p:eq(2)').html(questionBank.q1.c3)
 
     $('#set-input').on('click', function checkAnswer () {
-      // console.log("button clicked");
       var inputSubmitted = $('#input-field').val()
-      if (inputSubmitted === question.a) {
-        // console.log('You chose correctly')
+      if (inputSubmitted === questionBank.q1.a) {
         $('.resultDiv > p').html('Correct! Good Job')
       } else {
         $('.resultDiv > p').html('Sorry! Try again...')
       }
-      // console.log(inputSubmitted)
     })
   }
 
   printQuestion()
 })
 
-/* scrap
+/* TRIVIA GAME
+TO DO
+
+CORE
+- question bank with multiple questions
+- reset input screen to blank
+- allow input submission with enter key (not clicking on button)
+- score increments for correct answer, decrements for wrong answer
+
+CSS & bootstrap
+
+Deploy to Github
+
+*Bonus:*
+  * Add time-based scoring
+  * Track scores across games (even if the page is reloaded)
+  * Allow users to compete against each other on a high-score board.
+
+Optional
+  randomize questions
+  user can add questions
+
+-------------------------
+DONE
 Question properties
 - question has multiple possibleAnswers (1-3)
 - question has an single correctAnswer (1-3)
 
 Ask player for input
 If input matches AnswerKey then show "correct" and go to next question
-
-Optional
-score
-random questions
-add questions
-
-CSS
-- div with blank card
-- bootstrap
-
-Deploy
-- Github
 */
