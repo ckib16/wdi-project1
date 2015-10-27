c9b7acd5b1a825e234a680cfc15697616431a8a6
ckib16 <ckib16@gmail.com>
2015-10-27 11:25:08 -0400

    readme updated for completion of deliverable

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
 
     var score = 0
     $('.scoreDiv > p').html('Your score is ' + score)
-        
+
     var questionBank = {
       q1: {
         t: 'What is the definition of a "callback"?',
@@ -68,40 +68,3 @@ $(document).ready(function () {
   }
   playGame()
 })
-
-/* TRIVIA GAME TO DO
-
-Deliverables
-[x] 5 user stories
-[x] hosted on GH Pages
-[ ] HTML/CSS validated
-[x] lots of commits
-[ ] readme with explanations of the technologies used, the approach taken, installation instructions, unsolved problems, etc
-
-CORE
-- allow input submission with enter key (not clicking on button)
-- actual questions in bank
-
-CSS & bootstrap
-
-Deploy to Github
-
-*Bonus:*
-  * Allow users to compete against each other on a high-score board.
-  * Add time-based scoring
-  * Track scores across games (even if the page is reloaded)
-
-Optional
-  randomize questions
-  user can add questions
-
--------------------------
-DONE
-Question properties
-- question has multiple possibleAnswers (1-3)
-- question has an single correctAnswer (1-3)
-Ask player for input
-If input matches AnswerKey then show "correct" and go to next question
-- reset input screen to blank
-- score increments for correct answer, decrements for wrong answer
-*/
